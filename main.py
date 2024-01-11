841db0cdf29d350348cc259f61a8de37589f75d5
PopsicleSnow <popsiclehungry@gmail.com>
2024-01-10 17:37:47 -0800

    add clark kerr and finish index.html styling

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from flask import Flask, render_template
 
 #####
 """Get the menu items"""
+"""TODO:Use react for dining halls 
+        Add to database"""
 #####
 
 locations = {}
 today = datetime.now(pytz.timezone('America/Los_Angeles') ).strftime("%Y%m%d")
 locations["Cafe3"] = requests.get(f"https://dining.berkeley.edu/wp-content/uploads/xml_files/Cafe_3_{today}.xml")
+locations["ClarkKerr"] = requests.get(f"https://dining.berkeley.edu/wp-content/uploads/xml_files/Clark_Kerr_{today}.xml")
 locations["Foothill"] = requests.get(f"https://dining.berkeley.edu/wp-content/uploads/xml_files/Foothill_{today}.xml")
 locations["Crossroads"] = requests.get(f"https://dining.berkeley.edu/wp-content/uploads/xml_files/Crossroads_{today}.xml")
 
@@ -36,13 +39,19 @@ def cafe3():
 @app.route('/Crossroads')
 def crossroads():
     if "Crossroads" in locations:
-        return render_template('cafe3.html')
+        return render_template('crossroads.html')
     return render_template('closed.html')
 
 @app.route('/Foothill')
 def foothill():
     if "Foothill" in locations:
         return render_template('foothill.html')
+    return render_template('closed.html')
+
+@app.route('/ClarkKerr')
+def clarkkerr():
+    if "ClarkKerr" in locations:
+        return render_template('clarkkerr.html')
     return render_template('closed.html')
 
 if __name__ == '__main__':

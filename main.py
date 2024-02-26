6879c63b22f1e4777cc193cc33940669d6fd29fb
PopsicleSnow <popsiclehungry@gmail.com>
2024-02-26 13:55:14 -0800

    infinite error better fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from math import inf
+from math import inf, isinf
 from flask import Flask, render_template, jsonify, request
 from query_firestore import query
 from update_firestore import upload_menu_to_firestore
@@ -67,13 +67,16 @@ def result():
     # add up all the values
     data = {"calories": 0, "fat": 0, "carbs": 0, "protein": 0, "sugar": 0}
     for i in food.values():
+        if i[0] < 1 or isinf(i[0]):
+            i[0] = 0
+        i[0] = int(i[0])
         if len(i) != 6:
             return "Error"
-        data["calories"] += i[1] * i[0] if i[1] > 0 and i[1] != inf else 0
-        data["fat"] += i[2] * i[0] if i[2] > 0 and i[2] != inf else 0
-        data["carbs"] += i[3] * i[0] if i[3] > 0 and i[3] != inf else 0
-        data["protein"] += i[4] * i[0] if i[4] > 0 and i[4] != inf else 0
-        data["sugar"] += i[5] * i[0] if i[5] > 0 and i[5] != inf else 0
+        data["calories"] += i[1] * i[0] if i[1] > 0 and not isinf(i[1]) else 0
+        data["fat"] += i[2] * i[0] if i[2] > 0 and not isinf(i[2]) else 0
+        data["carbs"] += i[3] * i[0] if i[3] > 0 and not isinf(i[3]) else 0
+        data["protein"] += i[4] * i[0] if i[4] > 0 and not isinf(i[4]) else 0
+        data["sugar"] += i[5] * i[0] if i[5] > 0 and not isinf(i[5]) else 0
     return render_template('result.html', data=data)
 
 @app.route('/get_data')

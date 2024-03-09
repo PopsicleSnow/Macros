5050458f2fe0522ce37617554e000844440c7885
PopsicleSnow <popsiclehungry@gmail.com>
2024-03-08 18:52:18 -0800

    removed default value for food inputs

--- static/main.js
+++ static/main.js
@@ -31,7 +31,7 @@ function updateFoodItems(mealPeriod, data) {
         var name = foodItem["name"];
         $('#fooditems').append('<label for="' + foodItem["name"] + '" class="form-label">' + name + '</label>'
                                     + '<div class="input-group mb-3">'
-                                    + '<input type="number" name="' + name + '" min="0" max="50" value="0" aria-label="'
+                                    + '<input type="number" name="' + name + '" min="0" max="50" aria-label="'
                                     + name + '" class="form-control" id="' + name + '" placeholder="0">'
                                     + '<input type="hidden" name="' + name + '" value="' + foodItem["calories"] + '">'
                                     + '<input type="hidden" name="' + name + '" value="' + foodItem["fat"] + '">'

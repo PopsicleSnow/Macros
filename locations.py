8d5bccb6081257a381399632afbaad7dc2f39d4b
PopsicleSnow <popsiclehungry@gmail.com>
2024-10-21 23:21:34 -0700

    fixed api link

--- locations.py
+++ locations.py
@@ -18,10 +18,10 @@ def locations():
     """
     locations = {}
     today = datetime.now(pytz.timezone('America/Los_Angeles') ).strftime("%Y%m%d")
-    locations["Cafe3"] = requests.get(f"https://dining.berkeley.edu/wp-content/uploads/xml_files/Cafe_3_{today}.xml")
-    locations["ClarkKerr"] = requests.get(f"https://dining.berkeley.edu/wp-content/uploads/xml_files/Clark_Kerr_Campus_{today}.xml")
-    locations["Foothill"] = requests.get(f"https://dining.berkeley.edu/wp-content/uploads/xml_files/Foothill_{today}.xml")
-    locations["Crossroads"] = requests.get(f"https://dining.berkeley.edu/wp-content/uploads/xml_files/Crossroads_{today}.xml")
+    locations["Cafe3"] = requests.get(f"https://dining.berkeley.edu/wp-content/uploads/menus-exportimport/Cafe_3_{today}.xml")
+    locations["ClarkKerr"] = requests.get(f"https://dining.berkeley.edu/wp-content/uploads/menus-exportimport/Clark_Kerr_Campus_{today}.xml")
+    locations["Foothill"] = requests.get(f"https://dining.berkeley.edu/wp-content/uploads/menus-exportimport/Foothill_{today}.xml")
+    locations["Crossroads"] = requests.get(f"https://dining.berkeley.edu/wp-content/uploads/menus-exportimport/Crossroads_{today}.xml")
     for location in tuple(locations):
         if locations[location].status_code != 200:
             locations.pop(location)
